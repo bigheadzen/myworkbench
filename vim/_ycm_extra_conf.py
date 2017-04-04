69ab9b779aff8188d94a76cbf56e67f735890271
Jun Guo <imjunguo@gmail.com>
2017-04-04 21:58:03 +0800

    Update vimrc and YCM config.

--- vim/_ycm_extra_conf.py
+++ vim/_ycm_extra_conf.py
@@ -1,66 +1,32 @@
 import os
 import ycm_core
 
-# These are the compilation flags that will be used in case there's no # compilation database set (by default, one is not set).  # CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
+# These are the compilation flags that will be used in case there's no
+# compilation database set (by default, one is not set).
+# CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
-'-Wall',
-'-Wextra',
-'-Werror',
-'-Wc++98-compat',
-'-Wno-long-long',
-'-Wno-variadic-macros',
-'-fexceptions',
-'-DNDEBUG',
-# You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
-# source code needs it.
-'-DUSE_CLANG_COMPLETER',
-# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-# language to use when compiling headers. So it will guess. Badly. So C++
-# headers will be compiled as C headers. You don't want that so ALWAYS specify
-# a "-std=<something>".
-# For a C project, you would set this to something like 'c99' instead of
-# 'c++11'.
-'-std=c++11',
-# ...and the same thing goes for the magic -x option which specifies the
-# language that the files to be compiled are written in. This is mostly
-# relevant for c++ headers.
-# For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c++',
-'-isystem',
-'../BoostParts',
-'-isystem',
-# This path will only work on OS X, but extra paths that don't exist are not
-# harmful
-'/System/Library/Frameworks/Python.framework/Headers',
-'-isystem',
-'../llvm/include',
-'-isystem',
-'../llvm/tools/clang/include',
-'-I', '.', '-I',
-'./ClangCompleter',
-'-isystem',
-'./tests/gmock/gtest',
-'-isystem',
-'./tests/gmock/gtest/include',
-'-isystem',
-'./tests/gmock',
-'-isystem',
-'./tests/gmock/include',
-'-isystem',
-'/usr/bin/../lib/gcc/x86_64-linux-gnu/5.4.0/../../../../include/c++/5.4.0',
-'-isystem',
-'/usr/bin/../lib/gcc/x86_64-linux-gnu/5.4.0/../../../../include/x86_64-linux-gnu/c++/5.4.0',
-'-isystem',
-'/usr/bin/../lib/gcc/x86_64-linux-gnu/5.4.0/../../../../include/c++/5.4.0/backward',
-'-isystem',
-'/usr/local/include',
-'-isystem',
-'/usr/lib/llvm-3.8/bin/../lib/clang/3.8.0/include',
-'-isystem',
-'/usr/include/x86_64-linux-gnu',
-'-isystem',
-'/usr/include',
+    '-std=c++11',
+    '-O0',  
+    '-Werror', 
+    '-Weverything', 
+    '-Wno-documentation', 
+    '-Wno-deprecated-declarations', 
+    '-Wno-disabled-macro-expansion', 
+    '-Wno-float-equal', 
+    '-Wno-c++98-compat', 
+    '-Wno-c++98-compat-pedantic', 
+    '-Wno-global-constructors', 
+    '-Wno-exit-time-destructors', 
+    '-Wno-missing-prototypes', 
+    '-Wno-padded', 
+    '-Wno-old-style-cast',
+    '-Wno-weak-vtables',
+    '-x', 
+    'c++', 
+    '-I', 
+    '.', 
+    '-isystem', 
+    '/usr/include/', 
 ]
 
 
@@ -68,15 +34,11 @@ flags = [
 # compile_commands.json file to use that instead of 'flags'. See here for
 # more details: http://clang.llvm.org/docs/JSONCompilationDatabase.html
 #
-# You can get CMake to generate this file for you by adding:
-#   set( CMAKE_EXPORT_COMPILE_COMMANDS 1 )
-# to your CMakeLists.txt file.
-#
 # Most projects will NOT need to set this to anything; you can just change the
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
 compilation_database_folder = ''
 
-if os.path.exists( compilation_database_folder ):
+if compilation_database_folder:
   database = ycm_core.CompilationDatabase( compilation_database_folder )
 else:
   database = None
@@ -150,16 +112,11 @@ def FlagsForFile( filename, **kwargs ):
     final_flags = MakeRelativePathsInFlagsAbsolute(
       compilation_info.compiler_flags_,
       compilation_info.compiler_working_dir_ )
-
-    # NOTE: This is just for YouCompleteMe; it's highly likely that your project
-    # does NOT need to remove the stdlib flag. DO NOT USE THIS IN YOUR
-    # ycm_extra_conf IF YOU'RE NOT 100% SURE YOU NEED IT.
-    try:
-      final_flags.remove( '-stdlib=libc++' )
-    except ValueError:
-      pass
   else:
     relative_to = DirectoryOfThisScript()
     final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
 
-  return { 'flags': final_flags }
+  return {
+    'flags': final_flags,
+    'do_cache': True
+  }
